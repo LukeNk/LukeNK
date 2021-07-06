08c270250c3b07a5f55f50cf6845876e14036344
LukeNK <64694123+LukeNk@users.noreply.github.com>
2021-07-05 18:24:34 -0700

    Basic database

--- index.js
+++ index.js
@@ -1,25 +1,34 @@
 const fs = require('fs');
+const path = require('path');
 const http = require('http');
 
 function getFile(path, res) {
     fs.readFile(path, (err, data) => {
         if (err) {};
-        res.write((data) ? data : 'Cannot find');
-        res.end();
+        if (typeof(res) == 'object') {
+            res.write((data) ? data : 'Cannot find');
+            res.end();
+        } else if (typeof(res) == 'function') {
+            res(data);
+        }
     })
 }
 
 (function() {
     // initial, update APIs
     fs.readFile('API/song/list.html', 'utf8', (err, data) => {
-        fs.writeFile('API/song/count', (data.match(/<div class="song">/g) || []).length, 'utf8', () => {});
+        fs.writeFile('API/song/count', `${(data.match(/<div class="song"/g) || []).length}`, 'utf8', () => {});
     });
-
 })()
 
 let server = http.createServer((req, res) => {
     if (req.url == '/') {
         getFile('index.html', res);
+    } else if (path.dirname(req.url) == '/API/link') {
+        getFile('.' + req.url, (data) => {
+            res.writeHead(301, { "Location": data })
+            res.end();
+        });
     } else
         getFile('.' + req.url, res);
 });
